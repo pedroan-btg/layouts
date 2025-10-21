ca4a056abf60e180bc57dfd9769a1bbaac44d816
Anderson Pedro <anderson.pedro@btgpactual.com>
2025-10-21 05:32:28 -0300

    real api working

--- src/app/FormStepsComponent/contents/basic-info/services/get-deal-ras.service.ts
+++ src/app/FormStepsComponent/contents/basic-info/services/get-deal-ras.service.ts
@@ -10,15 +10,15 @@ export interface DealRasResponse {
 @Injectable({ providedIn: 'root' })
 export class GetDealRasService {
   private readonly http = inject(HttpClient);
-  private readonly baseUrl = 'http://sampleapi';
+  private readonly baseUrl = 'http://webapp-ras-uat/API/CreditRisk/RAS/RAS-API/External/core/workflow/deals/';
 
   /**
-   * Faz GET em `http://sampleapi/{dealRAS}/getdata`.
-   * @param dealRAS Valor do campo Deal RAS (ex.: "123")
+   * Faz GET em `http://webapp-ras-uat/API/CreditRisk/RAS/RAS-API/External/core/workflow/deals/48230?username=ras_system`.
+   * @param dealRAS Valor do campo Deal RAS (ex.: "48230")
    */
   getDealRas(dealRAS: string | number): Observable<DealRasResponse> {
     const id = String(dealRAS).trim();
-    const url = `${this.baseUrl}/${encodeURIComponent(id)}/getdata`;
+    const url = `${this.baseUrl}/${encodeURIComponent(id)}?username=ras_system`;
     return this.http.get<DealRasResponse>(url);
   }
 }
